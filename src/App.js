80c4ac6337c8cd965733265c2b4d140849ee287d
alenslyn <alenslyn@gmail.com>
2023-07-21 19:31:47 +0300

    feat: add portal and modal component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Books from "./components/books/Books";
+import Cart from "./components/books/Cart";
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Books />
--- src/components/books/Cart.js
+++ src/components/books/Cart.js
@@ -1,4 +1,6 @@
 import classes from "./Cart.module.css";
+import Modal from "./Modal";
+
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -15,7 +17,7 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -25,7 +27,7 @@ const Cart = (props) => {
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
